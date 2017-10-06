3e0170c0d9ee307781af8ff946e5acc1336897a7
恶魔狮子 <flychao4837@sina.com>
2017-10-07 00:11:37 +0800

    modify add new page

--- demo-test/czdk/js/app.js
+++ demo-test/czdk/js/app.js
@@ -1,61 +1,91 @@
 'use strict'
-$(function(){
-    /*录音页幻灯*/
-    var swiper1 = new Swiper('.swiper1', {
-        pagination : '.pagination1', //分页容器
-        loop:false,                  //无缝循环滚动
-        grabCursor: true,            //滑动时鼠标样式     
-        onImagesReady: function(){
-            console.log("onImagesReady");
+var app = {
+    win: $(window),
+    cookie: {
+        set: function (name, value, options) {
+            if (!options) {
+                options = options || {
+                };
+            }
+            var hour = options.hour ? options.hour : 0;
+            var path = options.path ? options.path + ';' : '/';
+            if (hour) {
+                var today = new Date();
+                var expire = new Date();
+                expire.setTime(today.getTime() + 3600000 * hour);
+            }
+            window.document.cookie = name + '=' + encodeURI(value) + '; ' + (hour ? ('expires=' + expire.toGMTString() + '; ')  : '') + ('path=' + path);
+            return true;
         },
-        onTransitionEnd:function(swiper1){
-        	var idx = swiper1.realIndex;
-        	var len = swiper1.slides.length;
-        	if(idx==0){
-            	$('.arrow-left').removeClass('active')
-	        }else{
-	            $('.arrow-left').addClass('active');
-	        }
-	        if(idx==len-1){
-	            $('.arrow-right').removeClass('active')
-	        }else{
-	            $('.arrow-right').addClass('active');
-	        }
-	        $(".pagination1 .counter>span").html((+swiper1.realIndex+1)+"/"+len);
+        get: function (name) {
+            var reg = new RegExp('(?:^|;+|\\s+)' + name + '=([^;]*)');
+            var m = window.document.cookie.match(reg);
+            return (!m ? '' : decodeURI(m[1]));
+        },
+        remove: function (name, path) {
+            var path = path ? path + ';' : '/';
+            window.document.cookie = name + '=; expires=Mon, 1 Jun 2014 01:00:00 GMT; ' + (path ? ('path=' + path + '; ')  : 'path=/; ');
         }
-    });
-    $('.arrow-left').click(function(e) {
-        var idx = swiper1.realIndex;
-        var len = swiper1.slides.length;
-        var that = $(this);
-        e.preventDefault();
-        
-        swiper1.slidePrev();
-        $(".pagination1 .counter>span").html((+swiper1.realIndex+1)+"/"+len);
-        $('.arrow-right').addClass('active')
-        if(idx==0|| idx==1){
-            that.removeClass('active')
-        }else{
-            that.addClass('active');
+    },
+    localStorage: {
+        set: function (key, value) {
+            if (window.localStorage) {
+                localStorage.setItem(key, value);
+            } else {
+                app.cookie.set(key, value);
+            }
+        },
+        get: function (key) {
+            if (window.localStorage) {
+                return localStorage.getItem(key);
+            } else {
+                return app.cookie.set(key);
+            }
+        },
+        remove: function (key) {
+            if (window.localStorage) {
+                localStorage.removeItem(key);
+            } else {
+                app.cookie.remove(key);
+            }
+        },
+        clear: function () {
+            if (window.localStorage) {
+                localStorage.clear();
+            } else {
+            }
         }
-    });
-    $('.arrow-right').click(function(e) {
-        var idx = swiper1.realIndex;
-        var len = swiper1.slides.length;
-        var that = $(this);
-        e.preventDefault();
-        swiper1.slideNext();
-        $(".pagination1 .counter>span").html((+swiper1.realIndex+1)+"/"+len);
-        $('.arrow-left').addClass('active');
-        if(idx==len-1|| idx==len-2){
-            that.removeClass('active')
-        }else{
-            that.addClass('active');
+    },
+    isMobile: function (str) {
+        var regu = /^[1][0-9]{10}$/;
+        var re = new RegExp(regu);
+        if (re.test(str)) {
+            return true;
+        } else {
+            return false;
         }
-    });
+    },
+    getQueryString : function(e) {
+        var t = new RegExp("(^|&)" + e + "=([^&]*)(&|$)"),
+            n = window.location.search.substr(1).match(t);
+        return null != n ? unescape(n[2]) : null
+    },
+    formatTime : function (t){
+        if(isNaN(t)){
+            return "00:00:00";
+        }
+        var h = parseInt(t/3600);
+        var m,s;
+        var dd = t%3600
+        m = parseInt(dd/60);
+        s = parseInt(dd%60);
+        h = h>9 ? h : "0"+h;
+        s = s>9 ? s : "0"+s;
+        s = s ==60? "00" : s;
+        m = s ==60? ++m :m;
 
-    $('.pagination1 .swiper-pagination-switch').click(function(){
-        swiper1.swipeTo($(this).index())
-    })
-
-})
+        m = m>9 ? m : "0"+m;
+        m = m ==60? "00" : m;
+        return h+":"+m+":"+s
+    }
+};
